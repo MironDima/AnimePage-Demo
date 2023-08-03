0ff1249bbb697b32645697bd42d64f40b77987e6
Miron Dima <mirontsov2098@mail.ru>
2023-08-03 12:54:05 +0300

    add modal and preloder

---
+++ src/script.js
@@ -0,0 +1,9 @@
+import setBg from "./modules/setBg";
+import modal from './modules/modal';
+import prelouder from './modules/prelouder';
+import search from './modules/search';
+
+setBg()
+prelouder()
+modal()
+search()
